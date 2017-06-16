14d6f05469f2bf9b392f31d2a5d6b04acecee885
marcus800 <samuelm@rgc.aberdeen.sch.uk>
2017-06-16 11:38:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 setup(name='sendgridEmailer',
       version='1.0',
       description='Send Email though sendgrid',
-      url='http://home',
+      url='https://github.com/marcus800/Sendgrid-emailer',
       author='Marcus Samuel',
       author_email='lewis@pinnaclevl.com',
-      license='none',
       packages=['sendgrid_emailer'],
       zip_safe=True)   
