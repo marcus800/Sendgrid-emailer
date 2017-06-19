1fb7334c28da75dcc45ca85e0aff8e47f6321d0e
marcus800 <samuelm@rgc.aberdeen.sch.uk>
2017-06-19 10:47:40 +0100

    Update Example.py

--- Example.py
+++ Example.py
@@ -2,7 +2,7 @@ import urllib
 from sendgrid_emailer import SendgridEmailer
 
 
-sending = SendgridEmailer("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")
+sending = SendgridEmailer("")
 sending.sendEmail("example@sentto.com", "example@sendfrom.com", "Bob", urllib.urlopen("Template1Example.html").read())
 
 print(" The email has been sent")
